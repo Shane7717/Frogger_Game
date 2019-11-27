95f7ea45c391622a277f199ffa98a551f5491d02
Xixuan Wang <scyxw3@exmail.nottingham.ac.uk>
2019-11-27 20:39:52 +0000

    Views update

--- src/xixuan/froggerapp/FroggerApp.java
+++ src/xixuan/froggerapp/FroggerApp.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 //Problems to be solved:
 //1. Should I add an SSH key to my project?
 //2. Should I delete my branches after finishing the whole project?
-//3. Should I include the "module"?
+//3. Should I include the "module"??
 //4. Should I add "." files into .gitignore?
 
 public class FroggerApp extends Application {
